44cbc3fb3225c0ffe993e64d7fb9f00267f248d5
Ohanyere Francis <mooref068@gmail.com>
2025-12-06 00:08:12 -0800

    feat: add  ActectureSection component to view the processess behind the project

---
+++ src/components/case-study/ArchitectureSection.tsx
@@ -0,0 +1,28 @@
+// src/components/case-study/ArchitectureSection.tsx
+import { motion } from "framer-motion";
+import StaggerSection from "../StaggerSection";
+import { fadeUp, scaleIn } from "../../animations/variants";
+
+export default function ArchitectureSection() {
+  return (
+    <StaggerSection>
+      <motion.section variants={fadeUp} className="mt-20">
+        <h2 className="text-2xl md:text-3xl font-bold tracking-tight px-4 pb-4">
+          Architecture Diagram
+        </h2>
+
+        <div className="bg-content-light dark:bg-content-dark p-6 rounded-xl shadow-md border border-border-light dark:border-border-dark">
+          <motion.img
+            variants={scaleIn}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: true }}
+            src="/caseStudy1.png"
+            alt="Architecture Diagram"
+            className="w-full h-auto rounded-lg"
+          />
+        </div>
+      </motion.section>
+    </StaggerSection>
+  );
+}
