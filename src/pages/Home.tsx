0f0905f4f85da68d3c3e971160c3d1b57a5a1b7a
Ohanyere Francis <mooref068@gmail.com>
2025-12-09 02:29:11 -0800

    feat: fix Home component breaking point

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex flex-1 justify-center py-5 sm:px-4 md:px-10 lg:px-20 xl:px-40">
-        <div className="flex flex-col max-w-[960px] flex-1">
+        <div className="flex flex-col max-w-[1200px] flex-1">
           <Header />
           <main className="flex-1">
             <Hero />
