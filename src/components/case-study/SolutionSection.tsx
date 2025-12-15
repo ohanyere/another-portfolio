e6a22e76ffa377c700704a0f6fc7b4635d0a5a0c
Ohanyere Francis <mooref068@gmail.com>
2025-12-15 03:24:47 -0800

    style : remove dark  class in the SolutionSection component

--- src/components/case-study/SolutionSection.tsx
+++ src/components/case-study/SolutionSection.tsx
@@ -4,10 +4,18 @@ import { fadeUp } from "../../animations/variants";
 import { useParams } from "react-router-dom";
 import { SiCheckmk } from "react-icons/si";
 {SiCheckmk}
+
 export default function SolutionSection() {
-   const {id} = useParams()
-      const githubLink = id?.includes("animal") ? "https://github.com/ohanyere/animal-check" : id?.includes("share") ? "https://github.com/ohanyere/share-interview" : id?.includes("graphql") ? "https://github.com/ohanyere/graphql-ecommerce" : id?.includes("monster") ? "https://github.com/ohanyere/monster" : null
-  
+  const { id } = useParams();
+  const githubLink = id?.includes("animal")
+    ? "https://github.com/ohanyere/animal-check"
+    : id?.includes("share")
+    ? "https://github.com/ohanyere/share-interview"
+    : id?.includes("graphql")
+    ? "https://github.com/ohanyere/graphql-ecommerce"
+    : id?.includes("monster")
+    ? "https://github.com/ohanyere/monster"
+    : null;
 
   return (
     <StaggerSection>
@@ -16,14 +24,16 @@ export default function SolutionSection() {
           The Solution
         </h2>
 
-        <div className="bg-content-light dark:bg-content-dark p-6 rounded-xl shadow-md border border-border-light dark:border-border-dark">
-          {
-            githubLink?.includes("animal") ? (<span>
+        <div className="bg-content-light p-6 rounded-xl shadow-md border border-border-light">
+          {githubLink?.includes("animal") ? (
+            <span>
               AnimalCheck provides an instant, AI-powered diagnosis system that allows farmers to upload images, describe symptoms, or send voice notes. The platform analyzes the input using advanced AI models and returns likely diseases along with treatment recommendations and prevention guidance. With multilingual support and 24/7 availability, AnimalCheck gives farmers an affordable, accessible, and fast way to protect their livestock and income.
-            </span>) : (<span>
+            </span>
+          ) : (
+            <span>
               PastWhiz AI solves this by allowing students to upload past questions, which the platform automatically processes and generates accurate answers with detailed explanations using AI models. It also organizes these questions into a structured, searchable database, helping students study efficiently by filtering questions by subject, topic, or year. This turns exam preparation into a faster, more effective, and more meaningful learning experience.
-            </span>)
-          }
+            </span>
+          )}
         </div>
       </motion.section>
     </StaggerSection>
